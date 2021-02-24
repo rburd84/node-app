dd763454e8627ec1f45d6741350f2676f47dd6a8
rburd84 <61717752+rburd84@users.noreply.github.com>
2021-02-24 17:17:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
 const tiger = require('tiger-lib')
+// methods
 const resultadd = tiger.add(3, 5)
-console.log(resultadd)
 const resultmulti = tiger.multiply(3, 5)
+
+// results
+console.log(resultadd)
 console.log(resultmulti)
